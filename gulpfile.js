f9f6d13548202a9499898d4ae786431775b4704d
Lina7227 <78968726+Lina7227@users.noreply.github.com>
2021-04-25 13:48:03 +0300

    ad

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,20 @@ const autoprefixer = require('gulp-autoprefixer')
 const browserSync = require('browser-sync')
 const imagemin = require('gulp-imagemin')
 
+const { src, dest } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+exports.default = function() {
+    return src('src/*.js')
+      .pipe(babel())
+      .pipe(src('vendor/*.js'))
+      .pipe(dest('output/'))
+      .pipe(uglify())
+      .pipe(rename({ extname: '.min.js' }))
+      .pipe(dest('output/'));
+} 
 function img(){
     gulp.src('./src/img/*.jpg')
         .pipe(imagemin([
@@ -19,7 +33,7 @@ function css(){
     .pipe(autoprefixer({
       browsers: ['last 4 versions']
     }))
-    .pipe(gulp.dest('./src/css/style.min.css'));
+    .pipe(gulp.dest('./src/css/min/*.min.css'));
 }
 
 function watch(){
@@ -32,6 +46,7 @@ function watch(){
 }
 
 gulp.watch('./src/index.html').on('change', browserSync.reload, gulp.series[watch, css]);
+gulp.watch('./src/css/style.css').on('change', browserSync.reload, css());
 
 exports.watch = watch;
 exports.small = img;
