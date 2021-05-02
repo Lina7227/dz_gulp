abbb95ef0462924d7f4dc78e389518dc714c5cc9
Lina7227 <78968726+Lina7227@users.noreply.github.com>
2021-05-02 21:47:32 +0300

    !

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,49 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass') 
-const concat = require('gulp-concat') 
-const cssnano = require('gulp-cssnano')
-const autoprefixer = require('gulp-autoprefixer')
-const browserSync = require('browser-sync')
-const imagemin = require('gulp-imagemin')
+const gulp = require("gulp");
+// const sass = require("gulp-sass");
+// const concat = require("gulp-concat");
+const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
 
-const { src, dest } = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
+const rename = require("gulp-rename");
+const { watch, series } = require("gulp");
 
-exports.default = function() {
-    return src('src/*.js')
-      .pipe(babel())
-      .pipe(src('vendor/*.js'))
-      .pipe(dest('output/'))
-      .pipe(uglify())
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(dest('output/'));
-} 
-function img(){
-    gulp.src('./src/img/*.jpg')
-        .pipe(imagemin([
-            imagemin.mozjpeg({quality: 75, progressive: true}),
-        ]))
-        .pipe(gulp.dest('./src/img_min'));
+function img() {
+    gulp
+      .src("./src/img/*.jpg")
+      .pipe(imagemin([imagemin.mozjpeg({ quality: 75, progressive: true })]))
+      .pipe(gulp.dest("./src/img_min/"));
 }
 
-function css(){
-    return  gulp.src('./src/css/style.css')
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions']
-    }))
-    .pipe(gulp.dest('./src/css/min/*.min.css'));
+function css() {
+    return gulp
+      .src("./src/css/style.css")
+      .pipe(autoprefixer())
+      .pipe(cssnano())
+      .pipe(rename({ extname: ".min.css" }))
+      .pipe(gulp.dest("./src/css/"));
 }
 
-function watch(){
+function watchOnHtml() {
     browserSync.init({
-        server:{
-            baseDir:'./src',
-            index: "/index.html"
-        }
+        server: {
+            baseDir: "./src",
+            index: "/index.html",
+        },
     });
 }
 
-gulp.watch('./src/index.html').on('change', browserSync.reload, gulp.series[watch, css]);
-gulp.watch('./src/css/style.css').on('change', browserSync.reload, css());
+// gulp.watch("./src/index.html").on("change", browserSync.reload, watch);
+// gulp.watch("./src/css/style.css").on("change", browserSync.reload, css);
 
-exports.watch = watch;
-exports.small = img;
+// exports.watch = watch;
+// exports.small = img;
+// exports.css = css;
+exports.default = function () {
+    watchOnHtml(), watch("./src/**/*").on("change", browserSync.reload);
+    watch("./src/img/*.*", img);
+    watch("./src/css/style.css", css);
+    // Or a composed task
+    // watch('./src/*.js', series(clean, javascript));
+  };
